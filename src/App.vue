7559a63b96b21057463436e4706ccee71725d78b
Iryna Suprun <irishka.am21@gmail.com>
2024-03-05 21:51:28 +0200

    created vue application and added header + first section and corresponding files for it

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppHeader />
+    <SectionNovelty />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import AppHeader from './components/AppHeader/index.vue';
+import SectionNovelty from './components/Sections/SectionNovelty/index.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    AppHeader,
+    SectionNovelty,
   },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" src="./styles/index.scss" />
---
+++ src/components/Sections/SectionNovelty/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="novelty">
+        <div class="novelty__box container">
+            <img src="../../../assets/new-coffee.png" alt="new-coffee picture"
+            class="novelty__picture">
+            <div class="novelty__describe">
+                <p class="novelty__before-title">What’s new?</p>
+                <h2 class="novelty__title">Meet our new coffee!</h2>
+                <p>Try one of our signature selections and see what everyone’s
+                talking about or select ‘Catalogue’ at the top of the
+                page to view all the possibilities.</p>
+                <a href="" class="novelty__click link">Learn more</a>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'SectionNovelty',
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
