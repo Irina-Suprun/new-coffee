cd94b0786dce5428a6e9333240b3b9b4e7624625
Iryna Suprun <irishka.am21@gmail.com>
2024-03-07 15:24:48 +0200

    created the footer(without responsive function

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <AppHeader />
     <SectionNovelty />
+    <AppFooter />
   </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader/index.vue';
 import SectionNovelty from './components/Sections/SectionNovelty/index.vue';
+import AppFooter from './components/AppFooter/index.vue';
 
 export default {
   name: 'App',
   components: {
     AppHeader,
     SectionNovelty,
+    AppFooter,
   },
 };
 </script>
---
+++ src/components/AppFooter/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <footer class="app-footer">
+    <div class="app-footer__content container">
+      <div class="app-footer__logo">
+        <strong>
+          <a href="" class="app-footer__logo link">
+          <img src="../../assets/footer-logo.svg" alt="picture-logo" class="app-footer__icon">
+          </a>
+        </strong>
+        <a href="" class="app-footer__after-logo link">Coffee shop</a>
+      </div>
+      <div class="app-footer__nav">
+        <ul class="app-footer__nav-list">
+          <li class="app-footer__item list">
+            <a href="" class="app-footer__link link">Catalogue</a>
+          </li>
+          <li class="app-footer__item list">
+            <a href="" class="app-footer__link link">Gift cards</a>
+          </li>
+          <li class="app-footer__item list">
+            <a href="" class="app-footer__link link">Delivery</a>
+          </li>
+        </ul>
+        <ul class="app-footer__nav-list">
+          <li class="app-footer__item list">
+            <a href="" class="app-footer__link link">About company</a>
+          </li>
+          <li class="app-footer__item list">
+            <a href="" class="app-footer__link link">Job opportunities</a>
+          </li>
+          <li class="app-footer__item list">
+            <a href="" class="app-footer__link link">Privacy Policy</a>
+          </li>
+          <li class="app-footer__item list">
+            <a href="" class="app-footer__link link">Terms of Use</a>
+          </li>
+          <li class="app-footer__item list">
+            <a href="" class="app-footer__link link">Contact Us</a>
+          </li>
+        </ul>
+        <div class="app-footer__social">
+          <p>Follow us</p>
+          <ul class="app-footer__social-list">
+            <li class="app-footer__item list">
+              <a href="" class="app-footer__social-link link">
+                <img src="../../assets/facebook.svg" alt="facebook" class="app-footer__social-icon">
+              </a>
+            </li>
+            <li class="app-footer__item list">
+              <a href="" class="app-footer__social-link link">
+                <img src="../../assets/instagram.svg" alt="instagram"
+                class="app-footer__social-icon">
+              </a>
+            </li>
+            <li class="app-footer__item list">
+              <a href="" class="app-footer__social-link link">
+                <img src="../../assets/twitter.svg" alt="twitter" class="app-footer__social-icon">
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <p class="app-footer__copyright">
+      &#169; 2023 CoffeeBuy. All rights reserved.
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter',
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
